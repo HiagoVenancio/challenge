0882c3c33c943404ee72ea1dc609f05f787426f5
rvhiago <hiago_rv@hotmail.com>
2024-04-27 21:36:13 +0100

    improving UX

--- app/src/main/java/com/example/projetoportugal/home/main_screen/MainScreen.kt
+++ app/src/main/java/com/example/projetoportugal/home/main_screen/MainScreen.kt
@@ -9,14 +9,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults.colors
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -34,6 +41,26 @@ fun MainScreen(
     viewModel: HomeViewModel,
     itemClick: (String) -> Unit
 ) {
+    var isLoading by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.width(64.dp),
+                color = MaterialTheme.colorScheme.secondary,
+                trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            )
+        }
+    }
+
+    viewModel.pokemonState.collectAsState().value.isNotEmpty().let {
+        isLoading = false
+
+    }
     Column(modifier = Modifier.padding(bottom = 25.dp)) {
         ListScreen(viewModel, itemClick)
     }
